6bc1424997aea83af6d914238d6850b3e5807bdc
Amish <myodamian@gmail.com>
2023-12-03 14:35:12 +0100

    Added function to visually compare methods

--- Analysis.py
+++ Analysis.py
@@ -57,3 +57,94 @@ def statistics(waiting, print_info=True):
         print(f'Average waiting time: {wait_avg} +- {wait_conf}')
 
     return wait_avg, wait_conf
+
+
+def plotComparison(rhos, save, names, colors, dpi=300):
+    """
+
+    :param rhos:
+    :param save:
+    :param names:
+    :param colors:
+    :param dpi:
+    :return:
+    """
+
+    # Create figure
+    fig, ax = plt.subplots()
+
+    # Load data
+    results = np.loadtxt(save)
+
+    # Loop through results
+    for i in range(results.shape[1] // 2):
+        # Average and confidence
+        average = results[:, i * 2]
+        conf = results[:, i * 2 + 1]
+
+        # Calculate left and right interval of confidence
+        left = average - conf
+        right = average + conf
+
+        # Plot results
+        ax.plot(rhos, average, color=colors[i], label=names[i])
+        ax.fill_between(rhos, left, right, color=colors[i], alpha=0.5)
+
+    # Plot settings
+    ax.grid()
+    ax.legend()
+    ax.set_xlabel('System load $\\rho$')
+    ax.set_ylabel('Average waiting time $W$')
+    fig.dpi = 300
+
+    return fig, ax
+
+
+def compare(rhos, params, methods, save, names, colors, dpi=300):
+    """
+    Function used to compare the average waiting time as a function of rho for multiple queuing systems. Data is also
+    saved using the save parameters.
+
+    :param rhos: All the rho's used to compare
+    :param params: Parameters for each method
+    :param methods: Function names of the queuing systems
+    :param save: Name and path of file to save to
+    :param names: Names of each method (used in legend)
+    :param colors: Colors used in plot for each method
+    :param dpi: DPI of figure
+    :return: Figure and axis object from matplotlib of the obtained plot
+    """
+
+    # Check if params and methods have same size
+    if len(params) != len(methods):
+        print('The size of parameters and methods are not the same')
+        return
+
+    # Array used to store results
+    results = []
+
+    # Loop through all rhos
+    for rho in rhos:
+        # Results for current rho
+        res = []
+        for method, param in zip(methods, params):
+            # Get waiting results
+            waiting = method(param[0], rho, *param[1:])
+
+            # Get the average and confidence interval
+            average, conf = statistics(waiting, False)
+
+            # Add to list
+            res += [average, conf]
+
+        # Add to results
+        results.append(res)
+
+    # Convert to numpy array
+    results = np.array(results)
+
+    # Save the results
+    np.savetxt(save, results)
+
+    # Plot and return figure and axis objects
+    return plotComparison(rhos, save, names, colors, dpi)
